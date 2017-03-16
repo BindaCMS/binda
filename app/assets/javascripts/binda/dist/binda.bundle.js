89ba107b0d4c0795bc2ea05cf26adda635c28e05
Alessandro Barbieri <info@alessandrobarbieri.net>
2017-03-16 18:51:08 +0100

    Imported all styles from previous CMS

--- app/assets/javascripts/binda/dist/binda.bundle.js
+++ app/assets/javascripts/binda/dist/binda.bundle.js
@@ -71,8 +71,8 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return message; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return Name; });
+/* unused harmony export message */
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return Name; });
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -105,9 +105,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__pages_js__ = __webpack_require__(0);
 
 
-alert(__WEBPACK_IMPORTED_MODULE_0__pages_js__["a" /* message */]);
-
-var John = new __WEBPACK_IMPORTED_MODULE_0__pages_js__["b" /* Name */]("John");
+var John = new __WEBPACK_IMPORTED_MODULE_0__pages_js__["a" /* Name */]("John");
 
 John.sayYourName();
 
--- app/assets/javascripts/binda/index.js
+++ app/assets/javascripts/binda/index.js
@@ -1,7 +1,5 @@
 import * as P from './pages.js'
 
-alert( P.message )
-
 let John = new P.Name("John")
 
 John.sayYourName()
