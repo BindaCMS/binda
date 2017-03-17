ce6b96d86c9cbb5ba61c61ef7083a0f36c69cfe8
Alessandro Barbieri <info@alessandrobarbieri.net>
2017-03-17 08:52:47 +0100

    FIX binda:install generator

--- lib/generators/binda/install/install_generator.rb
+++ lib/generators/binda/install/install_generator.rb
@@ -1,4 +1,4 @@
-class Binda::InstallGenerator < Rails::Generators::NamedBase
+class Binda::InstallGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
     def add_route
