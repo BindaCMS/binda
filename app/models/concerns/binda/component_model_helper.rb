e2d25373b319fee713091b42162e18c05a8d3872
Alessandro Barbieri <info@alessandrobarbieri.net>
2017-07-14 12:43:26 +0200

    FIX Binda::Binda on component model helper

--- app/models/concerns/binda/component_model_helper.rb
+++ app/models/concerns/binda/component_model_helper.rb
@@ -10,13 +10,13 @@ module Binda
 		# 
 		# @return [string] The content of the string/text or nil if it's empty or it doesn't exists
 		def get_text field_slug 
-			text = self.texts.find{ |t| t.field_setting_id == Binda::FieldSetting.get_id( field_slug ) }
+			text = self.texts.find{ |t| t.field_setting_id == FieldSetting.get_id( field_slug ) }
 			text.content unless text.nil?
 		end
 
 		# Get the object related to that field setting
 		def has_text field_slug 
-			obj = self.texts.find{ |t| t.field_setting_id == Binda::FieldSetting.get_id( field_slug ) }
+			obj = self.texts.find{ |t| t.field_setting_id == FieldSetting.get_id( field_slug ) }
 			if obj.present?
 				return !obj.content.blank?
 			else
@@ -26,7 +26,7 @@ module Binda
 
 		# Check if the field has an attached image
 		def has_image field_slug 
-			obj = self.assets.find{ |t| t.field_setting_id == Binda::FieldSetting.get_id( field_slug ) }.image
+			obj = self.assets.find{ |t| t.field_setting_id == FieldSetting.get_id( field_slug ) }.image
 			return obj.present?
 		end
 
@@ -40,7 +40,7 @@ module Binda
 
 		# Get the object related to that field setting
 		def get_image_info field_slug, size, info 
-			obj = self.assets.find{ |t| t.field_setting_id == Binda::FieldSetting.get_id( field_slug ) }
+			obj = self.assets.find{ |t| t.field_setting_id == FieldSetting.get_id( field_slug ) }
 			if obj.image.present?
 				if obj.image.respond_to?(size) && %w[thumb medium large].include?(size)
 					obj.image.send(size).send(info)
@@ -52,7 +52,7 @@ module Binda
 
 		# Check if the field has an attached date
 		def has_date field_slug 
-			obj = self.dates.find{ |t| t.field_setting_id == Binda::FieldSetting.get_id( field_slug ) }
+			obj = self.dates.find{ |t| t.field_setting_id == FieldSetting.get_id( field_slug ) }
 			if obj.present?
 				return !obj.date.nil?
 			else
@@ -62,30 +62,30 @@ module Binda
 
 		# Get the object related to that field setting
 		def get_date field_slug 
-			date = self.dates.find{ |t| t.field_setting_id == Binda::FieldSetting.get_id( field_slug ) }
+			date = self.dates.find{ |t| t.field_setting_id == FieldSetting.get_id( field_slug ) }
 			date.date unless date.nil?
 		end
 
 	  def has_repeater field_slug 
-	    obj = self.repeaters.find_all{ |t| t.field_setting_id == Binda::FieldSetting.get_id( field_slug ) }
+	    obj = self.repeaters.find_all{ |t| t.field_setting_id == FieldSetting.get_id( field_slug ) }
 	    return obj.present?
 	  end
 
 	  def get_repeater field_slug 
-	    repeater = self.repeaters.find_all{ |t| t.field_setting_id == Binda::FieldSetting.get_id( field_slug ) }
+	    repeater = self.repeaters.find_all{ |t| t.field_setting_id == FieldSetting.get_id( field_slug ) }
 	    repeater.sort_by(&:position) unless repeater.nil?
 	  end
 
 		def find_or_create_a_field_by field_setting_id, field_type
-			if Binda::FieldSetting.get_fieldables.include?( field_type.capitalize ) && field_setting_id.is_a?( Integer )
+			if FieldSetting.get_fieldables.include?( field_type.capitalize ) && field_setting_id.is_a?( Integer )
 				self.send( field_type.pluralize ).find_or_create_by( field_setting_id: field_setting_id )
 			else
-				raise ArgumentError, "A parameter of the method 'find_or_create_a_field_by' is not correct.", caller
+				raise ArgumentError, "One parameter of find_or_create_a_field_by() is not correct.", caller
 			end
 		end
 
 		def get_radio_choice field_slug
-			radio = self.radios.find{ |t| t.field_setting_id == Binda::FieldSetting.get_id( field_slug ) }
+			radio = self.radios.find{ |t| t.field_setting_id == FieldSetting.get_id( field_slug ) }
 			radio.get_choice() unless radio.nil?
 		end
 
