9e0feb930a3c1f0a223b8f7be56b67c0ed072995
Alessandro Barbieri <info@alessandrobarbieri.net>
2017-08-03 15:53:59 +0200

    FIX field setting issue when deleting from field group editor

--- app/controllers/binda/field_settings_controller.rb
+++ app/controllers/binda/field_settings_controller.rb
@@ -6,8 +6,6 @@ module Binda
     before_action :set_field_group
     before_action :set_field_setting, only: [:show, :edit, :update, :destroy]
 
-    include FieldableHelper
-
     def index
       @field_settings = @field_group.field_settings.order('position').all
     end
@@ -27,23 +25,23 @@ module Binda
       @field_setting = @field_group.field_settings.build(field_setting_params)
 
       if @field_setting.save
-        redirect_to structure_field_group_field_setting_path( @structure.slug, @field_group.slug, @field_setting.slug ), notice: 'Field setting was successfully created.'
+        redirect_to structure_field_group_field_setting_path( @structure, @field_group, @field_setting ), notice: 'Field setting was successfully created.'
       else
-        redirect_to new_structure_field_group_field_setting_path( @structure.slug, @field_group.slug ), flash: { alert: @field_setting.errors }
+        redirect_to new_structure_field_group_field_setting_path( @structure, @field_group ), flash: { alert: @field_setting.errors }
       end
     end
 
     def update
       if @field_setting.update(field_setting_params)
-        redirect_to structure_field_group_field_setting_path( @structure.slug, @field_group.slug, @field_setting.slug ), notice: 'Field setting was successfully updated.'
+        redirect_to structure_field_group_field_setting_path( @structure, @field_group, @field_setting ), notice: 'Field setting was successfully updated.'
       else
-        redirect_to edit_structure_field_group_field_setting_path( @structure.slug, @field_group.slug, @field_setting.slug ), flash: { alert: @field_setting.errors }
+        redirect_to edit_structure_field_group_field_setting_path( @structure, @field_group, @field_setting ), flash: { alert: @field_setting.errors }
       end
     end
 
     def destroy
       @field_setting.destroy
-      redirect_to structure_field_group_path( @structure.slug, @field_group.slug ), notice: 'Field setting and all dependent content were successfully destroyed.'
+      redirect_to structure_field_group_path( @structure, @field_group ), notice: 'Field setting and all dependent content were successfully destroyed.'
     end
 
     private
