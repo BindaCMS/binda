08c82400a67c3fcfddb58815725c566325c5f18d
Alessandro Barbieri <info@alessandrobarbieri.net>
2017-09-17 21:28:33 +0200

    FIX imperfections

--- app/controllers/concerns/binda/maintenance_helpers.rb
+++ app/controllers/concerns/binda/maintenance_helpers.rb
@@ -1,34 +1,34 @@
 module Binda
-  module MaintenanceHelpers
-    
-    extend ActiveSupport::Concern
+	module MaintenanceHelpers
+		
+		extend ActiveSupport::Concern
 
-    included do
-		  before_action :force_redirect
-		  layout :get_layout
+		included do
+			before_action :force_redirect
+			layout :get_layout
 		end
 
-	  def is_maintenance_mode
-	  	if get_board('dashboard').get_radio_choice('maintenance-mode')[:value] == 'true' && !user_signed_in?
+		def is_maintenance_mode
+			if get_board('dashboard').get_radio_choice('maintenance-mode')[:value] == 'true' && !user_signed_in?
 				return true
 			else
 				return false
 			end
-	  end
+		end
 
-	  private 
+		private 
 
-		  def get_layout
-		    if is_maintenance_mode
-		      'maintenance'
-		    else
-		      'application'
-		    end
-		  end
+			def get_layout
+				if is_maintenance_mode
+					'maintenance'
+				else
+					'application'
+				end
+			end
 
-	    def force_redirect
-	    	return redirect_to maintenance_path if request.original_url != maintenance_url && is_maintenance_mode
-	    end
+			def force_redirect
+				return redirect_to maintenance_path if request.original_url != maintenance_url && is_maintenance_mode
+			end
 
-  end
+	end
 end
