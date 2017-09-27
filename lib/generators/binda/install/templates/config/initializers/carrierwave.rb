8bae7d0c3cd10c1d9fb25ce1569e916f0620bacf
Alessandro Barbieri <info@alessandrobarbieri.net>
2017-09-27 17:32:45 +0200

    Add precious suggestions to carrierwave setup

--- lib/generators/binda/install/templates/config/initializers/carrierwave.rb
+++ lib/generators/binda/install/templates/config/initializers/carrierwave.rb
@@ -1,7 +1,10 @@
 CarrierWave.configure do |config|
   config.storage = :file
 
-  # # Use Google if you want
+
+  # # If you prefer to use Google uncomment and udpate the following lines
+  # # Make sure you read this before the setup
+  # # https://github.com/carrierwaveuploader/carrierwave#using-google-storage-for-developers
   # config.fog_provider = 'fog/google'                        # required
   # config.fog_credentials = {
   #   provider:                 'Google',
@@ -14,7 +17,8 @@ CarrierWave.configure do |config|
   # config.storage = :fog
   # config.fog_directory = ''
 
-  # # Use S3 if you want
+
+  # # If you prefer to use S3 uncomment and udpate the following lines
   # config.fog_credentials = {
   #   provider:               'AWS',
   #   region:                 '',
@@ -28,8 +32,10 @@ CarrierWave.configure do |config|
   # config.fog_public     = true
   # config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }
 
+
   # # Enable this line if you are using Heroku
   # config.cache_dir = "#{Rails.root}/tmp/uploads"
+
 
   config.enable_processing = !Rails.env.test?
 end
