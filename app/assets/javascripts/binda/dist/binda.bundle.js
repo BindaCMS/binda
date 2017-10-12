ca12743421a1a59f4eac3b938f12ef77317b8ae5
Alessandro Barbieri <info@alessandrobarbieri.net>
2017-10-12 10:11:41 +0200

    Cleaned jquery file upload script

--- app/assets/javascripts/binda/dist/binda.bundle.js
+++ app/assets/javascripts/binda/dist/binda.bundle.js
@@ -132,35 +132,33 @@ var _FormItemEditor = new FormItemEditor();
 
 "use strict";
 /* harmony export (immutable) */ __webpack_exports__["a"] = custom_fileupload;
-
 function custom_fileupload(target) {
 
 	var $this = $(target);
-	$this.fileupload({
 
+	// SETTINGS
+	// 
+	$this.fileupload({
 		url: $this.data('url'), // This should return json, not a proper page
 		dropZone: $this,
 		dataType: 'json',
 		autoUpload: true,
 		acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-		maxFileSize: 999000, // originally 999000
-		// Enable image resizing, except for Android and Opera,
-		// which actually support image resizing, but fail to
-		// send Blob objects via XHR requests:
-		disableImageResize: /Android(?!.*Chrome)|Opera/.test(window.navigator.userAgent),
-		previewMaxWidth: 100,
-		previewMaxHeight: 100,
-		previewCrop: true
+		maxFileSize: 999000 });
 
-	}).on('fileuploadadd', function (e, data) {
-
+	// ADD EVENT
+	// 
+	$this.on('fileuploadadd', function (e, data) {
 		data.context = $this.find('.details');
 		$.each(data.files, function (index, file) {
 			$('.fileupload--filename').text(file.name);
 		});
 		$('.fileupload--details').removeClass('fileupload--details--hidden');
-	}).on('fileuploadprocessalways', function (e, data) {
+	});
 
+	// PROCESS ALWAYS EVENT
+	// 
+	$this.on('fileuploadprocessalways', function (e, data) {
 		var index = data.index,
 		    file = data.files[index],
 		    node = $(data.context.children()[index]);
@@ -170,12 +168,19 @@ function custom_fileupload(target) {
 		if (index + 1 === data.files.length) {
 			data.context.find('button').text('Upload').prop('disabled', !!data.files.error);
 		}
-	}).on('fileuploadprogressall', function (e, data) {
+	});
+
+	// PROGRESS ALL EVENT
+	// 
+	$this.on('fileuploadprogressall', function (e, data) {
 
 		var progress = parseInt(data.loaded / data.total * 100, 10);
 		$('.fileupload--details .progress .progress-bar').css('width', progress + '%');
-	}).on('fileuploaddone', function (e, data) {
+	});
 
+	// DONE EVENT
+	// 
+	$this.on('fileuploaddone', function (e, data) {
 		$.each(data.result.files, function (index, file) {
 			if (file.url) {
 				setTimeout(function () {
@@ -195,19 +200,18 @@ function custom_fileupload(target) {
 				$(data.context.children()[index]).append('<br>').append(error);
 			}
 		});
-	}).on('fileuploadfail', function (e, data) {
+	});
 
+	// FAIL EVENT
+	// 
+	$this.on('fileuploadfail', function (e, data) {
 		$('.fileupload--details').addClass('fileupload--details--hidden');
-
 		alert('Uplaod failed');
+	});
 
-		// $.each(data.files, function (index) {
-		// var error = $('<span class="text-danger"/>').text('File upload failed.');
-		// $(data.context.children()[index])
-		// 	.append('<br>')
-		// 	.append(error);
-		// });
-	}).prop('disabled', !$.support.fileInput).parent().addClass($.support.fileInput ? undefined : 'disabled');
+	// what is this doing?
+	// not sure... see --> http://blueimp.github.io/jQuery-File-Upload/index.html
+	$this.prop('disabled', !$.support.fileInput).parent().addClass($.support.fileInput ? undefined : 'disabled');
 }
 
 /***/ }),
