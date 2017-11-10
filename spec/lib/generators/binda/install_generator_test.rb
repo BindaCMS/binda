f20ac722d832b4502739fbeef6248b735dba2cad
marco panzeri <marco.panzer9i@gmail.com>
2017-11-10 12:59:23 +0100

    REMOVE ammeter, MODIFY gitignore again

--- spec/lib/generators/binda/install_generator_test.rb
+++ spec/lib/generators/binda/install_generator_test.rb
@@ -7,9 +7,6 @@ module Binda
     destination Rails.root.join('tmp/generators')
     setup :prepare_destination
 
-    it 'should run all tasks in the generator' do
-      run_generator
-    end
 
     # test "generator runs without errors" do
     #   assert_nothing_raised do
