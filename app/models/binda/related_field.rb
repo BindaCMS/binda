c7f0f5ed81a254b6bfa037ee47e04d97b9738481
marco panzeri <marco.panzer9i@gmail.com>
2017-11-29 18:02:08 +0100

    ADD related boards

--- app/models/binda/related_field.rb
+++ app/models/binda/related_field.rb
@@ -5,10 +5,8 @@ module Binda
 
     # children_fieldable_relates "names" the Association join table for accessing through the children_fieldable association
     has_many :active_relationships, class_name: "Relationship", dependent: :destroy, as: :children_related
-=begin
     # parent_fieldable_relates "names" the Association join table for accessing through the parent_fieldable association
     has_many :passive_relationships, class_name: "Relationship", dependent: :destroy, as: :parent_related
-=end
 
 =begin
     # source: :children_fieldable matches with the belong_to :children_fieldable identification in the Association model
@@ -17,20 +15,16 @@ module Binda
     has_many :parent_fieldables, class_name: "Component", through: :active_relationships, source: :parent_fieldable, source_type: "Binda::Component"
 =end
 
-=begin
     # source: :children_fieldable matches with the belong_to :children_fieldable identification in the Association model
-    has_many :children_relateds, through: :passive_relationships, source: :children_related, source_type: "Binda::Component"
-=end
+    has_many :children_related_components, through: :passive_relationships, source: :children_related, source_type: "Binda::Component"
     # source: :parent_fieldable matches with the belong_to :children_fieldable identification in the Association model
-    has_many :parent_relateds, through: :active_relationships, source_type: "Binda::Component"
+    has_many :parent_related_components, through: :active_relationships, source: :parent_related, source_type: "Binda::Component"
 
 
-=begin
     # source: :children_fieldable matches with the belong_to :children_fieldable identification in the Association model
     has_many :children_related_boards, through: :passive_relationships, source: :children_related, source_type: "Binda::Board"
     # source: :parent_fieldable matches with the belong_to :children_fieldable identification in the Association model
     has_many :parent_related_boards, through: :active_relationships, source: :parent_related, source_type: "Binda::Board"
-=end
 
 =begin
     validates :parent_fieldable_id, presence: true
