fe1ad3e88c814b0f96d29199c0bd1e8011ce93d1
marco panzeri <marco.panzer9i@gmail.com>
2017-12-08 18:36:56 +0100

    FIX component layout, minot corrections to do

--- app/helpers/binda/components_helper.rb
+++ app/helpers/binda/components_helper.rb
@@ -3,9 +3,9 @@ module Binda
 
 		def get_sort_index_link
 			if @structure.components.length < Component.sort_limit
-				link_to "Sort #{ @structure.name.humanize.split.map(&:capitalize).join(' ').pluralize }", structure_components_sort_index_path, class: 'main-header--link b-btn b-btn-'
+				link_to "<i class=\"fa fa-random\" aria-hidden=\"true\"></i>Sort #{ @structure.name.humanize.split.map(&:capitalize).join(' ').pluralize }".html_safe, structure_components_sort_index_path, class: 'main-header--link b-btn b-btn-primary'
 			else
-				link_to "Sort #{ @structure.name.humanize.split.map(&:capitalize).join(' ').pluralize }", '#', class: 'main-header--link btn btn-default', onclick: "alert(\"Sorry! It's not possible to sort #{@structure.name.pluralize} anymore. You currently have more than #{Component.sort_limit} #{@structure.name.pluralize} which is the maximum limit.\")"
+				link_to "Sort #{ @structure.name.humanize.split.map(&:capitalize).join(' ').pluralize }", '#', class: 'main-header--link b-btn b-btn-primary', onclick: "alert(\"Sorry! It's not possible to sort #{@structure.name.pluralize} anymore. You currently have more than #{Component.sort_limit} #{@structure.name.pluralize} which is the maximum limit.\")"
 			end
 		end
 
