d24e960fba7b3586227f209fcb3dcf34dbc4d34d
marco panzeri <marco.panzer9i@gmail.com>
2017-12-11 13:54:39 +0100

    FIX table

--- spec/dummy/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-
-	mount Binda::Engine => "/admin_panel"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
