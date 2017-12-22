ca280e4201f5c50ea7fe6e08fe99dd040913ba89
Alessandro Barbieri <info@alessandrobarbieri.net>
2017-12-22 18:45:34 +0100

    DUMB mistake. Removed new migrations

--- db/migrate/20171222172222_change_owner_column_for_relation_links.rb
+++
@@ -1,5 +0,0 @@
-class ChangeOwnerColumnForRelationLinks < ActiveRecord::Migration[5.1]
-  def change
-  	remove_column :binda_relation_links, :owner_type
-  end
-end
--- db/migrate/20171222173308_add_index_to_owner.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToOwner < ActiveRecord::Migration[5.1]
-  def change
-  	add_index :binda_relation_links, :owner_id
-  end
-end
