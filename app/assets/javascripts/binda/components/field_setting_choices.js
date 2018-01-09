c9f4558d29cee4bdc5c4577250458c1289065b25
Alessandro Barbieri <info@alessandrobarbieri.net>
2018-01-09 11:55:30 +0100

    Separate fielg group parent settings from repeaters ones

--- app/assets/javascripts/binda/components/field_setting_choices.js
+++ app/assets/javascripts/binda/components/field_setting_choices.js
@@ -5,11 +5,11 @@
 import { _FormItemEditor } from './form_item_editor'
 
 
-class FormItemChoice {
+class FieldSettingChoices {
 	
 	constructor()
 	{
-		this.target = '.form-item--choice'
+		this.target = '.field-setting-choices--choice'
 	}
 
 	isSet()
@@ -20,20 +20,20 @@ class FormItemChoice {
 
 	setEvents()
 	{
-		$(document).on('click', '.form-item--add-choice', addChoice )
+		$(document).on('click', '.field-setting-choices--add-choice', addChoice )
 		
-		$(document).on('click', '.form-item--delete-choice', deleteChoice )
-		$(document).on('click', '.form-item--js-delete-choice', function( event )
+		$(document).on('click', '.field-setting-choices--delete-choice', deleteChoice )
+		$(document).on('click', '.field-setting-choices--js-delete-choice', function( event )
 		{
 			event.preventDefault()
-			$( this ).closest('.form-item--choice').remove()
+			$( this ).closest('.field-setting-choices--choice').remove()
 			// Update form item editor size
 			_FormItemEditor.resize()
 		})
 	}
 }
 
-export let _FormItemChoice = new FormItemChoice()
+export let _FieldSettingChoices = new FieldSettingChoices()
 
 
 /**
@@ -44,10 +44,11 @@ function addChoice(event)
 {
 	event.preventDefault()
 	// Clone the new choice field
-	var choices = $( this ).closest('.form-item--choices')
-	var newchoice = choices.find('.form-item--new-choice')
-	var clone = newchoice.clone().removeClass('form-item--new-choice').toggle()
-	clone.find('.form-item--toggle-choice').toggle()
+	var choices_id = $( this ).data('choices-id')
+	var choices = $(`#${choices_id}`)
+	var newchoice = choices.find('.field-setting-choices--new-choice')
+	var clone = newchoice.clone().removeClass('field-setting-choices--new-choice').toggle()
+	clone.find('.field-setting-choices--toggle-choice').toggle()
 	// Append the clone right after
 	choices.prepend( clone )
 	// Update form item editor size
@@ -59,7 +60,7 @@ function deleteChoice( event )
 {
 	event.preventDefault()
 
-	var choice = $( this ).closest('.form-item--choice')
+	var choice = $( this ).closest('.field-setting-choices--choice')
 	var destination = $( this ).attr('href')
 	var self = this
 
--- app/assets/javascripts/binda/index.js
+++ app/assets/javascripts/binda/index.js
@@ -2,29 +2,29 @@
 /// INDEX OF BINDA'S SCRIPTS
 ///- - - - - - - - - - - - - - - - - - - -
 
-import { _FormItem }         from './components/form_item'
-import { _FormItemRepeater } from './components/form_item_repeater'
-import { _FormItemImage }    from './components/form_item_image'
-import { _FormItemChoice }   from './components/form_item_choice'
-import { _FormItemEditor }   from './components/form_item_editor'
-import { _FileUpload }       from './components/fileupload'
-import { _Shader }           from './components/login-shader'
-import { _LoginForm }        from './components/login_form'
-import setupSortable         from './components/sortable'
-import setupFieldGroupEditor from './components/field_group_editor'
-import setupBootstrap        from './components/bootstrap'
-import setupSelect2          from './components/select2'
-import setupRadioToggle      from './components/radio-toggle'
+import { _FormItem }                 from './components/form_item'
+import { _FormItemRepeater }         from './components/form_item_repeater'
+import { _FormItemImage }            from './components/form_item_image'
+import { _FieldSettingChoices }      from './components/field_setting_choices'
+import { _FormItemEditor }           from './components/form_item_editor'
+import { _FileUpload }               from './components/fileupload'
+import { _Shader }                   from './components/login-shader'
+import { _LoginForm }                from './components/login_form'
+import setupSortable                 from './components/sortable'
+import setupFieldGroupEditor         from './components/field_group_editor'
+import setupBootstrap                from './components/bootstrap'
+import setupSelect2                  from './components/select2'
+import setupRadioToggle              from './components/radio-toggle'
 
 $(document).ready( function()
 {
-	if ( _FormItem.isSet() )         { _FormItem.setEvents() }
-	if ( _FormItemRepeater.isSet() ) { _FormItemRepeater.setEvents() }
-	if ( _FormItemImage.isSet() )    { _FormItemImage.setEvents() }
-	if ( _FormItemChoice.isSet() )   { _FormItemChoice.setEvents() }
-	if ( _FormItemEditor.isSet() )   { _FormItemEditor.setEvents() }
-	if ( _FileUpload.isSet() )       { _FileUpload.setEvents() }
-  if ( _LoginForm.isSet() )        { _LoginForm.setEvents() }
+	if ( _FormItem.isSet() )                 { _FormItem.setEvents() }
+	if ( _FormItemRepeater.isSet() )         { _FormItemRepeater.setEvents() }
+	if ( _FormItemImage.isSet() )            { _FormItemImage.setEvents() }
+	if ( _FieldSettingChoices.isSet() )   { _FieldSettingChoices.setEvents() }
+	if ( _FormItemEditor.isSet() )           { _FormItemEditor.setEvents() }
+	if ( _FileUpload.isSet() )               { _FileUpload.setEvents() }
+  if ( _LoginForm.isSet() )                { _LoginForm.setEvents() }
   if ( _Shader.isSet() ) 
   {
 		_Shader.setup()
