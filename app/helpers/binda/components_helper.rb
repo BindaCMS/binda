1b097e22e86f71623db95b415b497af42bb7804e
Alessandro Barbieri <info@alessandrobarbieri.net>
2018-01-18 14:01:11 +0100

    Remove all glyphicons, better components index sorting

--- app/helpers/binda/components_helper.rb
+++ app/helpers/binda/components_helper.rb
@@ -5,6 +5,7 @@ module Binda
 		#
 		# This helper generate the a link to the page where to sort components, or 
 		#   a broken link that tells you that you have too many components to go to that page.
+		#   
 		def get_sort_index_link
 			if @structure.components.length < Component.sort_limit
 				link_to "<i class=\"fa fa-random\" aria-hidden=\"true\"></i>Sort #{ @structure.name.humanize.split.map(&:capitalize).join(' ').pluralize }".html_safe, structure_components_sort_index_path, class: 'main-header--link b-btn b-btn-primary'
@@ -17,6 +18,7 @@ module Binda
 		# 
 		# This helper return the field description (as `string`) or false (as `boolean`)
 		#   in order to tell Simple Form whether to generate or not the hint html tag.
+		#   
 		def prepare_description_for_form_hint field_setting
 			if field_setting.description.blank?
 				return false
@@ -24,6 +26,47 @@ module Binda
 				return field_setting.description
 			end
 		end
+
+		# Get sort link by argument
+		#
+		# This method returns a URL which contains the current sort options
+		#   and update just the title one. The URL then is used to change the 
+		#   sorting of the index in which is used.
+		#  
+		# Rails guide reference --> http://guides.rubyonrails.org/action_controller_overview.html#hash-and-array-parameters
+		# 
+		# @param arg [string] This should be the database column on which sort will be based
+		# @return [string] The URL with the encoded parameters
+		# 
+    def get_sort_link_by arg
+      if params[:order].nil? 
+      	structure_components_path( [@structure], order: {"#{arg}": "ASC"} )
+      else
+      	order_hash = params[:order].permit(:name, :publish_state).to_h
+      	if order_hash[arg] == "ASC"
+	        order_hash[arg] = "DESC"
+	        structure_components_path( [@structure], order: order_hash  )
+	      else
+	      	order_hash[arg] = "ASC"
+	        structure_components_path( [@structure], order: order_hash  )
+	      end
+	    end
+    end
+
+		# Get sort link icon by argument
+		#
+		# This method returns a Font Awesome icon 
+		# 
+		# @param arg [string] This should be the database column on which sort will be based
+		# @return [string] The icon which needs to be escaped with the `html_safe` method
+		# 
+    def get_sort_link_icon_by arg
+      if !params[:order].nil? && params[:order][arg] == "ASC"
+        '<i class="fas fa-sort-alpha-down"></i>'
+      else
+        '<i class="fas fa-sort-alpha-up"></i>'
+      end
+    end
 
 	end
 end
