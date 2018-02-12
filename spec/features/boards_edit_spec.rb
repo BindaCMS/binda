53a28c8ed2fb3d0394974b808ce5758f13a83793
Alessandro Barbieri <info@alessandrobarbieri.net>
2018-02-12 21:54:03 +0100

    trying to get Travis not to complain

--- spec/features/boards_edit_spec.rb
+++ spec/features/boards_edit_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe "GET component#edit", type: :feature, js: true do
+describe "GET boards#edit", type: :feature, js: true do
   include CarrierWave::Test::Matchers
 
 	let(:user){ Binda::User.first }
@@ -24,6 +24,8 @@ describe "GET component#edit", type: :feature, js: true do
 		string_id = @board.strings.where(field_setting_id: string_setting.id).first.id
 		string_field = "board_strings_attributes_#{string_id}_content"
 		string_value = 'oh my lorem'
+		expect(page).to have_content(string_setting.name)
+		expect(page).to have_field(string_field)
 		find_field(string_field)
 		fill_in string_field, with: string_value
 		click_button "save"
@@ -33,29 +35,21 @@ describe "GET component#edit", type: :feature, js: true do
 
 	it "allows to edit a string field in a repeater" do
 		ids = @board.repeaters.first.string_ids
-
 		repeater_expand_btn = "#form--list-item-#{@board.repeaters.first.id} .form-item--collapse-btn"
-
 		find(repeater_expand_btn).click
 		# wait animation
 		sleep 1
-
 		string_field = "board_repeaters_attributes_#{@board.repeaters.first.id}_strings_attributes_#{ids[ids.length-1]}_content"
 		string_value = 'oh my lorem'
 		find("##{string_field}")
-
 		fill_in string_field, with: string_value
 		click_button "save"
-
 		visit @path
-
-
 		find(repeater_expand_btn).click
 		# wait animation
 		sleep 1
 		# there is something that causes a error and sleeping for a seconds seems to avoid it...
 		sleep 2
-
 		expect(page).to have_field(string_field)
 		expect(page).to have_field(string_field, with: string_value)		
 	end
@@ -104,50 +98,38 @@ describe "GET component#edit", type: :feature, js: true do
 
 	it "allows to add an image to an image field and store it" do
     Binda::Image::ImageUploader.enable_processing = true
-
 		# Create an image field setting on which will work
 		image_setting = create(:image_setting, field_group_id: @structure.field_groups.first.id)
-		
 		# Refresh the page so the image field appear on the editor
 		visit @path
-
 		expect(@board.images.first.image.present?).not_to be_truthy
-
 		field_id = "board_images_attributes_#{@board.images.where(field_setting_id: image_setting.id ).first.id}_image"
 		image_name = 'test-image.jpg'
 		image_path = ::Binda::Engine.root.join('spec', 'support', image_name)
 		page.execute_script("document.getElementById('#{field_id}').style.zIndex = '1'")
 		page.execute_script("document.getElementById('#{field_id}').style.opacity = '1'")
 		page.attach_file(field_id, image_path)
-		
 		wait_for_ajax
 		sleep 1 # wait for animation to complete
-	
 		@board.reload
 		image = @board.images.first
-
 		within "#fileupload-#{image.id}" do
 			expect(page).to have_content image_name
 		end
-
     if CarrierWave::Uploader::Base.storage == CarrierWave::Storage::File
       file = MiniMagick::Image.open(::Rails.root.join(image.image.path))
     else
       file = MiniMagick::Image.open(image.image.url)
     end
-
 		within "#fileupload-#{image.id}" do
 			expect( page ).to have_content file.width
 		end
-
 		visit @path
-
 		expect( File.basename( image.image.path ) ).to eq image_name
 		within "#fileupload-#{image.id}" do
 			expect( page ).to have_content image_name
 			expect( page ).to have_content file.width
 		end
-
     Binda::Image::ImageUploader.enable_processing = false
 	end
 
