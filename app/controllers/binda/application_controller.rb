862264c39134583742bd958d50cb15ac4f0da17a
Alessandro Barbieri <info@alessandrobarbieri.net>
2018-02-23 17:37:44 +0100

    Make sure Binda uses english even though is mounted on a app with a differnt localization setup

--- app/controllers/binda/application_controller.rb
+++ app/controllers/binda/application_controller.rb
@@ -4,6 +4,7 @@ module Binda
     protect_from_forgery with: :exception
 
 		before_action :authenticate_user!
+		before_action :set_locale
 
 		# _ indicates that we are not using the argument in the method
 	  def after_sign_in_path_for(_)
@@ -13,6 +14,11 @@ module Binda
 		# _ indicates that we are not using the argument in the method
 	  def after_sign_out_path_for(_)
 	  	root_path
+	  end
+
+	  # Avoid inheriting locale from application on which Binda is mounted
+	  def set_locale
+	  	I18n.locale = I18n.default_locale
 	  end
 
   end
