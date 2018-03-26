35209df77bd60253fcac2009989fd7bf425dae5f
marco panzeri <marco.panzer9i@gmail.com>
2018-03-26 10:23:47 +0200

    ADD svg helpers

--- app/models/concerns/binda/fieldable_association_helpers.rb
+++ app/models/concerns/binda/fieldable_association_helpers.rb
@@ -292,6 +292,56 @@ module Binda
 			end
 		end
 
+			# Check if the field has an attached audio
+		# 
+		# @param field_slug [string] The slug of the field setting
+		# @return [boolean]
+		def has_svg(field_slug)
+			obj = self.svgs.find{ |t| t.field_setting_id == FieldSetting.get_id( field_slug ) }
+			# Alternative query
+			# obj = Image.where(field_setting_id: FieldSetting.get_id( field_slug ), fieldable_id: self.id, fieldable_type: self.class.to_s ).first
+			raise ArgumentError, "There isn't any svg associated to the current slug (#{field_slug}) on instance (#{self.class.name} ##{self.id}).", caller if obj.nil?
+			return obj.svg.present?
+		end
+
+		# Get the svg url based on the size provided, 
+		#   default is Carrierwave default (usually the real size)
+		# 
+		# @param field_slug [string] The slug of the field setting
+		# @param size [string] The size. It can be 'thumb' 200x200 cropped, 
+		#   'medium' 700x700 max size, 'large' 1400x1400 max size, or blank
+		# @return [string] The url of the svg
+		def get_svg_url(field_slug)
+			get_svg_info( field_slug, 'url' )
+		end
+
+		# Get the svg path based on the size provided, 
+		#   default is Carrierwave default (usually the real size)
+		# 
+		# @param field_slug [string] The slug of the field setting
+		# @param size [string] The size. It can be 'thumb' 200x200 cropped, 
+		#   'medium' 700x700 max size, 'large' 1400x1400 max size, or blank
+		# @return [string] The url of the svg
+		def get_svg_path(field_slug)
+			get_svg_info( field_slug, 'path' )
+		end
+
+		# Get the object related to that field setting
+		# 
+		# @param field_slug [string] The slug of the field setting
+		# @param info [string] String of the info to be retrieved
+		# @return [string] The info requested if present
+		# @return [boolean] Returns false if no info is found or if image isn't found
+		def get_svg_info(field_slug, info)
+			obj = self.svgs.find{ |t| t.field_setting_id == FieldSetting.get_id( field_slug ) }
+			# Alternative query
+			# obj = svg.where(field_setting_id: FieldSetting.get_id( field_slug ), fieldable_id: self.id, fieldable_type: self.class.to_s ).first
+			raise ArgumentError, "There isn't any svg associated to the current slug (#{field_slug}) on instance (#{self.class.name} ##{self.id}).", caller if obj.nil?
+			if obj.svg.present?
+				obj.svg.send(info)
+			end
+		end
+
 		# Check if the field has an attached date
 		# 
 		# @param field_slug [string] The slug of the field setting
