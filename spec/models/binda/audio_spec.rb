545c852cb105e3b78a4bbd8884699fe6b85f58f9
marco panzeri <marco.panzer9i@gmail.com>
2018-09-06 10:07:09 +0200

    ADD passing tests for audio_spec

--- spec/models/binda/audio_spec.rb
+++ spec/models/binda/audio_spec.rb
@@ -27,5 +27,51 @@ module Binda
       expect(audio_record.content_type).to eq 'audio/mpeg'
       expect(audio_record.file_size).to be_within(100000).of(400000) # audio is 300kb
     end
+
+    describe "when is read only" do
+      it "blocks any upload" do
+        @audio_setting.read_only = true
+        @audio_setting.save!
+        expect(@audio_setting.reload.read_only).to be(true)
+        audio_name = 'test-audio.mp3'
+        audio_path = ::Binda::Engine.root.join('spec', 'support', audio_name)
+        audio_record = @component.reload.audios.first
+        audio_record.audio = audio_path.open
+        expect{ audio_record.save! }.to raise_error ActiveRecord::RecordInvalid
+      end
+      describe "when there is already a image" do
+        it "avoid audio to be deleted" do
+          audio_name = 'test-audio.mp3'
+          audio_path = ::Binda::Engine.root.join('spec', 'support', audio_name)
+          audio_record = @component.reload.audios.first
+          audio_record.audio = audio_path.open
+          expect(audio_record.save!).to be_truthy
+          @audio_setting.read_only = true
+          @audio_setting.save!
+          expect(@audio_setting.reload.read_only).to be(true)
+          audio_name = 'test-audio.mp3'
+          audio_path = ::Binda::Engine.root.join('spec', 'support', audio_name)
+          audio_record = @component.reload.audios.first
+          audio_record.audio = audio_path.open
+          audio_record.remove_audio
+          expect{ audio_record.save! }.to raise_error ActiveRecord::RecordInvalid
+        end
+        it "blocks any update" do
+          audio_name = 'test-audio.mp3'
+          audio_path = ::Binda::Engine.root.join('spec', 'support', audio_name)
+          audio_record = @component.reload.audios.first
+          audio_record.audio = audio_path.open
+          expect(audio_record.save!).to be_truthy
+          @audio_setting.read_only = true
+          @audio_setting.save!
+          expect(@audio_setting.reload.read_only).to be(true)
+          audio_name = 'test-audio.mp3'
+          audio_path = ::Binda::Engine.root.join('spec', 'support', audio_name)
+          audio_record = @component.reload.audios.first
+          audio_record.audio = audio_path.open
+          expect{ audio_record.save! }.to raise_error ActiveRecord::RecordInvalid
+        end
+      end
+    end
   end
 end
