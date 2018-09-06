d4c48c0efccb17fc0ed363f5e4e7c07e164d6fba
marco panzeri <marco.panzer9i@gmail.com>
2018-09-06 09:56:42 +0200

    ADD passing tests for svg_spec

--- spec/models/binda/svg_spec.rb
+++ spec/models/binda/svg_spec.rb
@@ -2,29 +2,77 @@ require 'rails_helper'
 
 module Binda
   RSpec.describe Svg, type: :model do
-  include CarrierWave::Test::Matchers
 
-	before(:context) do
-	  SvgUploader.enable_processing = true
-	end
+		include CarrierWave::Test::Matchers
 
-	before(:example) do
-	  @component = create(:component)
-	  @svg_setting = create(:svg_setting, field_group_id: @component.structure.field_groups.first.id)
-	end
+		before(:context) do
+			SvgUploader.enable_processing = true
+		end
 
-	after(:context) do
-	  SvgUploader.enable_processing = false
-	end
+		before(:example) do
+			@component = create(:component)
+			@svg_setting = create(:svg_setting, field_group_id: @component.structure.field_groups.first.id)
+		end
 
-	it "stores content_type and file size" do
-	  svg_name = 'test-svg.svg'
-	  svg_path = ::Binda::Engine.root.join('spec', 'support', svg_name)
-	  svg_record = @component.reload.svgs.first
-	  svg_record.svg = svg_path.open
-	  expect(svg_record.save!).to be_truthy
-	  expect(svg_record.content_type).to eq 'image/svg+xml'
-	  expect(svg_record.file_size).to be_within(400).of(600) # audio is 300kb
+		after(:context) do
+			SvgUploader.enable_processing = false
+		end
+
+		it "stores content_type and file size" do
+			svg_name = 'test-svg.svg'
+			svg_path = ::Binda::Engine.root.join('spec', 'support', svg_name)
+			svg_record = @component.reload.svgs.first
+			svg_record.svg = svg_path.open
+			expect(svg_record.save!).to be_truthy
+			expect(svg_record.content_type).to eq 'svg/svg+xml'
+			expect(svg_record.file_size).to be_within(400).of(600) # audio is 300kb
+		end
+
+		describe "when is read only" do
+			it "blocks any upload" do
+				svg_name = 'test-svg.svg'
+				svg_path = ::Binda::Engine.root.join('spec', 'support', svg_name)
+				svg_record = @component.reload.svgs.first
+				svg_record.svg = svg_path.open
+        @svg_setting.read_only = true
+        @svg_setting.save!
+        expect(@svg_setting.reload.read_only).to be(true)
+        expect{ svg_record.save! }.to raise_error ActiveRecord::RecordInvalid
+			end
+			describe "when there is already a svg" do
+				it "avoid svg to be deleted" do
+					svg_name = 'test-svg.svg'
+					svg_path = ::Binda::Engine.root.join('spec', 'support', svg_name)
+					svg_record = @component.reload.svgs.first
+					svg_record.svg = svg_path.open
+					expect(svg_record.save!).to be_truthy
+					@svg_setting.read_only = true
+          @svg_setting.save!
+          expect(@svg_setting.reload.read_only).to be(true)
+          svg_name = 'test-svg.svg'
+          svg_path = ::Binda::Engine.root.join('spec', 'support', svg_name)
+          svg_record = @component.reload.svgs.first
+          svg_record.svg = svg_path.open
+          svg_record.remove_svg
+          expect{ svg_record.save! }.to raise_error ActiveRecord::RecordInvalid
+				end
+				it "blocks any update" do
+					svg_name = 'test-svg.svg'
+					svg_path = ::Binda::Engine.root.join('spec', 'support', svg_name)
+					svg_record = @component.reload.svgs.first
+					svg_record.svg = svg_path.open
+					expect(svg_record.save!).to be_truthy
+					@svg_setting.read_only = true
+          @svg_setting.save!
+          expect(@svg_setting.reload.read_only).to be(true)
+					svg_name = 'test-svg.svg'
+					svg_path = ::Binda::Engine.root.join('spec', 'support', svg_name)
+					svg_record = @component.reload.svgs.first
+					svg_record.svg = svg_path.open
+          expect{ svg_record.save! }.to raise_error ActiveRecord::RecordInvalid
+				end
+			end
+		end
+
 	end
-  end
 end
