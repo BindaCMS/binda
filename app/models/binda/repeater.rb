e2380b0c14b33a78a7f9ddb8dd0369064a34ba72
Alessandro Barbieri <info@alessandrobarbieri.net>
2019-05-27 15:50:01 +0200

    Clean code

--- app/models/binda/repeater.rb
+++ app/models/binda/repeater.rb
@@ -5,16 +5,16 @@ module Binda
     include Fields
 
     # The following direct association is used to securely delete associated fields
-    # Infact via `fieldable` the associated fields might not be deleted 
+    # Infact via `fieldable` the associated fields might not be deleted
     # as the fieldable_id is related to the `component` rather than the `field_setting`
 
     after_create :set_default_position
 
     # Set default position after create
-    # 
+    #
     # This methods ensure that every repeater instance has an explicit position.
     #   The latest repeater created gets the highest position number.
-    # 
+    #
     # @return [object] Repeater instance
     def set_default_position
         Repeater
